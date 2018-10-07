6e33960ce162eabf713a98fa4325138cee2dfd52
Henry Hu <henry.hu.sh@gmail.com>
2018-10-07 18:11:22 -0400

    fix readlink

--- Telegram/SourceFiles/storage/storage_encrypted_file_tests.cpp
+++ Telegram/SourceFiles/storage/storage_encrypted_file_tests.cpp
@@ -21,7 +21,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include <thread>
 #ifdef Q_OS_MAC
 #include <mach-o/dyld.h>
-#elif defined Q_OS_LINUX // Q_OS_MAC
+#elif defined Q_OS_LINUX || defined Q_OS_FREEBSD // Q_OS_MAC
 #include <unistd.h>
 #endif // Q_OS_MAC || Q_OS_LINUX
 
