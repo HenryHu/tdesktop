a7b375c24b16f05baefc5a43df56f79f2feff99d
Henry Hu <henry.hu.sh@gmail.com>
2019-09-07 22:34:14 -0400

    for old version

--- Telegram/SourceFiles/chat_helpers/stickers_emoji_pack.cpp
+++ Telegram/SourceFiles/chat_helpers/stickers_emoji_pack.cpp
@@ -103,7 +103,7 @@ constexpr auto kClearSourceTimeout = 10 * crl::time(1000);
 		},
 		5,
 	};
-	static const auto list = std::array{
+	static const auto list = std::array<const Lottie::ColorReplacements*, 5>{
 		&color1,
 		&color2,
 		&color3,
